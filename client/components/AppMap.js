2463e849a45763c907db45a120f5843f5b9527b6
Garrett Green <garrett.wile.green@gmail.com>
2018-11-14 14:44:09 -0600

    mapbox installed

--- client/components/AppMap.js
+++ client/components/AppMap.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react'
-import { HereMap } from 'rc-here-maps'
+import { render } from 'react-dom'
+import MapGL from 'react-map-gl'
 
 const AppMap = () => {
+
+  const MAPBOX_TOKEN = 'pk.eyJ1IjoiZ2FycmV0dGdyZWVuIiwiYSI6ImNqb2htMzU4ajAxOW0za3Q0ejFqdW84M3UifQ.ttT97UAbxDQqXw64WCIsNA';
+
+
+  const viewport = {
+    latitude: 41.8781,
+    longitude: -87.6298,
+    zoom: 14,
+    bearing: 0,
+    pitch: 0
+  }
+
   return (
     <div className="map-flex">
-      <div className="map-container">
-        <div className="renderOverMap" />
-        <HereMap
-          appId="sdtDQZ6Dfg5N4JEfnqJk"
-          appCode="4X46Htqh77dq_eujY5Kc1g"
-          center={{ lat: 41.895366, lng: -87.639037 }}
-          zoom={14}
-          useHTTPS
-        />
-      </div>
+      <MapGL
+        {...viewport}
+        width="100vw"
+        height="100vh"
+        mapStyle="mapbox://styles/mapbox/streets-v10"
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+      />
     </div>
   )
 }
