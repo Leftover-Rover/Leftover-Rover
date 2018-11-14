f78136084764e242ac7775dced2c1547b66450be
Daniel Eimer <danieleimer1@gmail.com>
2018-11-14 15:18:50 -0600

    initial db seed

--- client/components/AppMap.js
+++ client/components/AppMap.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
-import MapGL from 'react-map-gl'
+import MapGL, { Marker } from 'react-map-gl'
 
 const AppMap = () => {
-
-  const MAPBOX_TOKEN = 'pk.eyJ1IjoiZ2FycmV0dGdyZWVuIiwiYSI6ImNqb2htMzU4ajAxOW0za3Q0ejFqdW84M3UifQ.ttT97UAbxDQqXw64WCIsNA';
-
+  const MAPBOX_TOKEN =
+    'pk.eyJ1IjoiZ2FycmV0dGdyZWVuIiwiYSI6ImNqb2htMzU4ajAxOW0za3Q0ejFqdW84M3UifQ.ttT97UAbxDQqXw64WCIsNA'
 
   const viewport = {
     latitude: 41.8781,
