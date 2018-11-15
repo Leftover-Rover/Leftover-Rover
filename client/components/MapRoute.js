365642140f96e9876658343241a516b3c570732e
Nathan Avilez <tungsteno22@gmail.com>
2018-11-15 15:11:15 -0600

    the user can geolocate

--- client/components/MapRoute.js
+++ client/components/MapRoute.js
@@ -1,55 +1,33 @@
 import React from 'react'
-import mapboxgl from 'mapbox-gl'
-import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
+import UserMap from './UserMap'
+import { geolocated } from 'react-geolocated'
 
-// import 'mapbox-gl/dist/mapbox-gl.css' // Updating node module will keep css up to date.
-// import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css' // Updating node module will keep css up to date.
-
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiZ2FycmV0dGdyZWVuIiwiYSI6ImNqb2htMzU4ajAxOW0za3Q0ejFqdW84M3UifQ.ttT97UAbxDQqXw64WCIsNA'
-
-export default class Map extends React.Component {
-  componentDidMount = () => {
-    const { lng, lat, zoom } = {
-      lng: -87.6298,
-      lat: 41.8781,
-      zoom: 13.4
+class Map extends React.Component {
+  state = {
+    origin: '',
+    destination: '',
+    centerLat: 41.8781,
+    centerLng: -87.6298
+  }
+  componentDidUpdate(prevProps) {
+    if (this.props.coords !== prevProps.coords) {
+      this.setState({
+        centerLng: this.props.coords.longitude,
+        centerLat: this.props.coords.latitude
+      })
     }
-
-    const map = new mapboxgl.Map({
-      container: this.mapContainer, // See https://blog.mapbox.com/mapbox-gl-js-react-764da6cc074a
-      style: 'mapbox://styles/mapbox/streets-v9',
-      center: [lng, lat],
-      zoom
-    })
-
-    this.directions = new MapboxDirections({
-      accessToken: mapboxgl.accessToken,
-      unit: 'metric',
-      profile: 'mapbox/driving',
-      // interactive: false,
-      controls: {
-        inputs: false,
-        instructions: false
-      }
-    })
-
-    map.addControl(this.directions, 'top-left')
-    // directions.setOrigin('405 W Superior St, Chicago IL 60654')
-    // directions.setDestination('3838 N Fremont St, Chicago IL 60613')
   }
 
   handleRoute = () => {
-    this.directions.setOrigin('405 W Superior St, Chicago IL 60654')
-    this.directions.setDestination('3838 N Fremont St, Chicago IL 60613')
+    this.setState({
+      origin: '405 W Superior St. Chicago, IL 60654',
+      destination: '3838 N Fremon St. Chicago, IL 60613'
+    })
   }
-
   render() {
     return (
       <React.Fragment>
-        <div>
-          <div ref={el => (this.mapContainer = el)} className="map" />
-        </div>
+        <UserMap {...this.state} />
         <span>
           <button type="button" onClick={this.handleRoute}>
             Press ME
@@ -59,3 +37,5 @@ export default class Map extends React.Component {
     )
   }
 }
+
+export default Map
