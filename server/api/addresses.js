045384a021591ed86121085c8624d1d4c4625209
Garrett Green <garrett.wile.green@gmail.com>
2018-11-19 10:43:33 -0600

    completed update or add default dropoff

---
+++ server/api/addresses.js
@@ -0,0 +1,14 @@
+// const router = require('express').Router()
+// const { Order, User, Driver, Address } = require('../db/models')
+// module.exports = router
+
+// // Route for adding new default address
+// router.post('/add', async (req, res, next) => {
+
+//   try {
+
+//     res.json(order)
+//   } catch (err) {
+//     next(err)
+//   }
+// })
--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { User } = require('../db/models')
+const { User, Address } = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
@@ -13,5 +13,25 @@ router.get('/', async (req, res, next) => {
     res.json(users)
   } catch (err) {
     next(err)
+  }
+})
+
+router.put('/:userId/address', async (req, res, next) => {
+  try {
+    let user = await User.findById(req.params.userId)
+    if (!user) {
+      const err = new Error('User Not Found Or No User Is Logged In')
+      err.status = 500
+      return next(err)
+    } else {
+      const updatedUser = await user.update({
+        ...user,
+        defaultDeliveryLat: req.body.newDefaultLat,
+        defaultDeliveryLng: req.body.newDefaultLng
+      })
+      res.json(updatedUser)
+    }
+  } catch (err) {
+    console.error(err)
   }
 })
