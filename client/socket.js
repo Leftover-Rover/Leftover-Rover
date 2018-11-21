fdb364898da13db8a74c4aed9089f958e3ab0655
Daniel Eimer <danieleimer1@gmail.com>
2018-11-21 15:37:37 -0600

    will now only send socket requests to 5 nearest active drivers

--- client/socket.js
+++ client/socket.js
@@ -5,10 +5,16 @@ import store, { updateActionItem } from './store'
 const socket = io(window.location.origin)
 
 driverEvent.on('driverIsActive', () => {
+  console.log(store.getState())
   socket.on('driverRequest', (order, driverList) => {
-    console.log('update action item:', updateActionItem, 'store: ', store)
-    store.dispatch(updateActionItem())
-    console.log('do you want this Rover?', order, driverList)
+    // Checking here to see if I am one of the drivers in the driverList sent from the server. If I am, then I will get the updated action item.
+    driverList.forEach(driver => {
+      if (driver.id === store.getState().user.driver.id) {
+        console.log('update action item:', updateActionItem, 'store: ', store)
+        store.dispatch(updateActionItem())
+        console.log('do you want this Rover?', order, driverList)
+      }
+    })
   })
 })
 
