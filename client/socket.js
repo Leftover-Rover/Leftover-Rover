51aae381609544c117cf8c34aec790e8ef6fa24c
Nathan Avilez <tungsteno22@gmail.com>
2018-11-23 10:49:56 -0500

    remove console logs

--- client/socket.js
+++ client/socket.js
@@ -7,7 +7,6 @@ const socket = io(window.location.origin)
 driverEvent.on('driverIsActive', () => {
   socket.on('driverRequest', (order, driverList) => {
     // Checking here to see if I am one of the drivers in the driverList sent from the server. If I am, then I will get the updated action item.
-    console.log(driverList)
     driverList.forEach(driver => {
       if (driver.id === store.getState().user.driver.id) {
         store.dispatch(updateActionItem())
