dea7520402ed33b1facff4247452c74d077ca7b8
Mark Bassett <bassmarkie@gmail.com>
2018-11-26 16:35:46 -0600

    also deleted old UserHome and all references

--- client/components/index.js
+++ client/components/index.js
@@ -4,7 +4,6 @@
  * having to figure out which file they belong to!
  */
 
-export { default as UserHome } from './user-home'
 export { Login, Signup } from './auth-form'
 export { default as Rover } from './Rover'
 export { default as Me } from './Me'
