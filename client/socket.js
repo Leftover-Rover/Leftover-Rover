76a2e40a1d17e1c0c6c7e61d1c3793e05a7e93a0
Nathan Avilez <tungsteno22@gmail.com>
2018-11-29 13:48:17 -0600

    erase a couple of console.logs

--- client/socket.js
+++ client/socket.js
@@ -8,17 +8,13 @@ const socket = io(window.location.origin)
 export const orderInfo = { orderForDriver: {}, driverList: [] }
 
 driverEvent.on('driverIsActive', () => {
-  console.log('ready for orders')
   socket.on('driverRequest', (orderForDriver, driverList) => {
     // Checking here to see if I am one of the drivers in the driverList sent from the server. If I am, then I will get the updated action item.
-    console.log('order offered to', driverList)
-    // store.dispatch(updateActionItem())
     driverList.forEach(driver => {
       if (driver.id === store.getState().user.driver.id) {
         store.dispatch(updateActionItem())
         orderInfo.orderForDriver = orderForDriver
         orderInfo.driverList = driverList
-        console.log('do you want this Rover?', orderInfo)
       }
     })
   })
