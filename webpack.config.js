096d7269f81b9e4c216f57f7d8aa72e7eb1ddd7c
Nathan Avilez <tungsteno22@gmail.com>
2019-02-10 20:35:42 -0600

    fixed security warining

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const isDev = process.env.NODE_ENV === 'development'
 
 module.exports = {
-  mode: isDev ? 'development' : 'production',
   entry: [
     '@babel/polyfill', // enables async-await
     './client/index.js'
